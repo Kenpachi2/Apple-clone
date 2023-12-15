bd80d8474d2e79582497b9b4dd3bfe201dcda0db
Kenpachi2 <ifeanyiumeadi201@gmail.com>
2023-12-15 10:29:35 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='mb-[6rem]'>
+      <Header />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="bg-neutral-700 h-11 fixed w-full top-0  z-[10000000] py-[0.6rem]">
+
+    </div>
+  )
+}
+
+export default Header
